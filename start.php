bc022b252f4aeffaca84441488df8d425a083be8
LauraH24 <heringlaura@web.de>
2019-12-04 22:18:24 +0100

    Added start.php

---
+++ start.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>FashionFriends</title>
+		<meta charset ='utf-8'>
+	</head>
+	<body>
+		<h1>Willkommen bei FashionFriends!</h1></br></br>
+		<h3>Neu hier? Registrier dich!</h3></br></br>
+		<button type='button' class='button'>Login</button>
+		<button type='button' class='button'>Registrieren</button>
+	</body>
+</html>
