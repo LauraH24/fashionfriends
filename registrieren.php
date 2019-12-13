79449527f6c5115c616110028bd4c623be994d70
LauraH24 <heringlaura@web.de>
2019-12-13 23:19:26 +0100

    Änderungen Datei-Upload

--- registrieren.php
+++ registrieren.php
@@ -7,7 +7,7 @@
 		<?php
 			if(!isset($_POST['senden']))
 			{
-				echo "<form action='' method='POST'>";
+				echo "<form action='' method='POST' ectype='multipart/form-data'>";
 				echo "Vorname: <input type='text' name='vorname'></br>";
 				echo "Nachname: <input type='text' name='nachname'></br>";
 				echo "Benutzername: <input type='text' name='benutzername'></br>";
@@ -16,42 +16,47 @@
 				echo "Passwort wiederholen: <input type='password' name='passwied'></br>";
 				echo "<input type='submit' name='senden' value='Senden'>";
 				echo "<input type='reset' name='loeschen' value='Löschen'>";
-				echo "</form>";
-				echo "<form action='upload.php' method='POST' enctype='multipart/form-data'>";
 				echo "<input type='file' name='profilbild'>";
 				echo "</form>";
 				
 			}
 			else
 			{
-				$id = date('Y-m-d, H:i:s');
+				$db = mysqli_connect('localhost','root','','bbs');
+				
 				$vorname = $_POST['vorname'];
 				$nachname = $_POST['nachname'];
 				$benutzername = $_POST['benutzername'];
 				$email = $_POST['email'];
 				$pass = $_POST['pass'];
 				$passwied = $_POST['passwied'];
-				$profilbild = $_POST['profilbild'];
+				$name = $_FILES['profilbild']['name'];
+				$type = $_FILES['profilbild']['type'];
+				$profilbild = file_get_contents($_FILES['profilbild']['tmp_name']);
+				$stmt = $db->prepare("insert into ffbenutzer values('',?,?,?)");
+				$stmt->bindParam(1,$name);
+				$stmt->bindParam(2,$type);
+				$stmt->bindParam(3,$profilbild);
+				$stmt->execute();
 				
 				if($pass != $passwied)
 				{
 					echo "Die Passwörter stimmen nicht überein! Bitte versuche es erneut. </br>";
 					echo "<a href='registrieren.php'>Erneut versuchen</a>";
 				}
-			$db = mysql_connect('localhost','root','','bbs');
 			$sql = "SELECT benutzername FROM ffbenutzer WHERE benutzername=$benutzername";
-			$res = mysql_query($db, $sql);
-			$num = mysql_num_rows($res);
+			$res = mysqli_query($db, $sql);
+			$num = mysqli_num_rows($res);
 			if ($num == 1)
 			{
 				echo "Dieser Benutzername existiert bereits. Bitte wähle einen anderen Benutzernamen aus.</br>";
 				echo "<a href='registrieren.php'>Erneut versuchen</a>";
 			}
-			$insert = "INSERT INTO ffbenutzer VALUES ('$id','$vorname','$nachname','$benutzername','$email','$pass','$profilbild')";
-			$inres = mysql_query($db, $insert);
-			$innum = mysql_query($inres);
+			$insert = "INSERT INTO ffbenutzer VALUES ('$vorname','$nachname','$benutzername','$email','$pass','$profilbild')";
+			$inres = mysqli_query($db, $insert);
+			$innum = mysqli_query($inres);
 			
-			mysql_close($db);
+			mysqli_close($db);
 			}
 		?>
 	</body>
--- upload.php
+++ upload.php
@@ -1,3 +1,3 @@
 <?php
-	move_upload_file($_FILES['bild']['tmp_name'], 'upload/'.$_FILES['bild']['tmp_name']);
+	move_uploaded_file($_FILES['profilbild']['tmp_name'], 'upload/'.$_FILES['profilbild']['tmp_name']);
 ?>
