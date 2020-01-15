94897b68c536f07b22fe6d5a933d10bdfba23f20
LauraH24 <heringlaura@web.de>
2020-01-15 10:20:39 +0100

    Bildupload

--- beitrag_erstellen.php
+++ beitrag_erstellen.php
@@ -2,18 +2,65 @@
 	<head>
 		<title>FF - Beitrag erstellen</title>
 		<meta charset='utf-8'>
-		<link href='style_start.css' rel='stylesheet'>
+		<link href='style_beitragerstellen.css' rel='stylesheet'>
+		<link rel="shortcut icon" type="image/x-icon" href="logo_neu.png">
 	</head>
 	<body>
+	<h1>Beitrag erstellen</h1>
 		<?php
-			if (!isset($_POST['senden']))
-			{
-				echo "<form action='upload.php' method='POST' enctype='multipart/form-data'>";
-				echo "<input type='file' name='bild'></br>";
-				echo "<input type='text' name='beschreibung' placeholder='Bildbeschreibung hinzufügen'></br>";
-				echo "<input type='submit' name='senden' value='Erstellen'>";
-				echo "</form>";
-			}
-		?>
+			$db = mysqli_connect("localhost","root","","bbs");
+			
+  // Initialize message variable
+  $msg = "";
+
+  // If upload button is clicked ...
+  if (isset($_POST['upload'])) {
+  	// Get image name
+  	$image = $_FILES['image']['name'];
+  	// Get text
+  	$image_text = mysqli_real_escape_string($db, $_POST['image_text']);
+
+  	// image file directory
+  	$target = "images/".basename($image);
+
+  	$sql = "INSERT INTO ffupload (image, image_text) VALUES ('$image', '$image_text')";
+  	// execute query
+  	mysqli_query($db, $sql);
+
+  	if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+  		$msg = "Image uploaded successfully";
+  	}else{
+  		$msg = "Failed to upload image";
+  	}
+  }
+  $result = mysqli_query($db, "SELECT * FROM ffupload");
+?>
+<div id="content">
+  <?php
+    while ($row = mysqli_fetch_array($result)) {
+      echo "<div id='img_div'>";
+      	echo "<img src='images/".$row['image']."' >";
+      	echo "<p>".$row['image_text']."</p>";
+      echo "</div>";
+    }
+  ?>
+  <form method="POST" action="upload2.php" enctype="multipart/form-data">
+  	<input type="hidden" name="size" value="1000000">
+  	<div>
+  	  <input type="file" name="image">
+  	</div>
+  	<div>
+      <textarea 
+      	id="text" 
+      	cols="40" 
+      	rows="4" 
+      	name="image_text" 
+      	placeholder="Say something about this image..."></textarea>
+  	</div>
+  	<div>
+  		<button type="submit" name="upload">Upload</button>
+  	</div>
+  </form>
+</div>
 	</body>
 </html>
--- upload.php
+++
@@ -1,3 +0,0 @@
-<?php
-	move_uploaded_file($_FILES['profilbild']['tmp_name'], 'upload/'.$_FILES['profilbild']['tmp_name']);
-?>
