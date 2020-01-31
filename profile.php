2ee46b78aa042bb03eea83db40c6769fa2548fb4
LauraH24 <heringlaura@web.de>
2020-01-31 10:31:38 +0100

    TO DO

--- profile.php
+++ profile.php
@@ -9,14 +9,21 @@
 			session_start();
 			if(!isset($_SESSION['email']))
 			{
-				echo "Bitte logge dich erst ein!";
-				echo "<a href='login.php'>Login</a>";
+				echo "<h1>Bitte logge dich erst ein!</h1></br>";
+				echo "<a href='login.php'>Login</a></br></br>";
+				echo "<h3>Noch nicht registriert? Melde dich jetzt an!</h3></br>";
 				echo "<a href='registrieren.php'>Registrieren</a>";
 			}
 			else
 			{
 				$email = $_SESSION['email'];
 				$passwort = $_SESSION['passwort'];
+				
+				echo "<div id='profilbild'>";
+				echo "<div id='benutzername'>";
+				echo "<div id='bio'>";
+				echo "<table>";
+				echo "<th></th>";
 				
 				echo "<a href='logout.php'>Logout</a>";
 			}
